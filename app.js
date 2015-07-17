17f309dd1a9bd043346da54f7232774b9f72ba78
Claudio Di Giovanni <claudio.digiovanni@gmail.com>
2015-07-17 13:22:03 +0200

    test

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -25,23 +27,12 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 var products = require('./myProductRoutes');
 app.use('/api', products);
 
-
-
-
-var server = app.listen(80, function () {
-
-// Connection URL
-//var url = 'mongodb://0.0.0.0:27017/myproject';
-var url = 'mongodb://root:root@ds047772.mongolab.com:47772/mymongodb'
-mongoosex.connect(url);
-
-
-
-
-var host = server.address().address;
-var port = server.address().port;
-console.log('Example app listening at http://%s:%s', host, port);
-
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+  var url = 'mongodb://root:root@ds047772.mongolab.com:47772/mymongodb'
+   mongoosex.connect(url);
 });
+
+
 
 
